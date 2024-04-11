9b6b64316db1f1388e79863bfadb6b2ceb399163
Aniket Jadhav <aaniket20@gmail.com>
2024-04-11 19:58:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,8 @@ import './index.css'
 
 import MyApp from './MyApp'
 
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
